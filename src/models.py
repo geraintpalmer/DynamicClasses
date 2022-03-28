155447040131db2344141b3f9a40d107d258f7d5
Geraint Palmer <palmer.geraint@googlemail.com>
2022-03-28 21:37:38 +0100

    add MC models, simulation model, and comparison

---
+++ src/models.py
@@ -0,0 +1,127 @@
+import numpy as np
+import itertools
+
+# Import Ciw from source - required features not merged yet.
+import sys
+sys.path.append("../../")
+import CiwPython.Ciw.ciw as ciw
+import CiwPython.Ciw.ciw.dists as dists
+import CiwPython.Ciw.ciw.trackers as trackers
+
+
+class MarkovChain:
+    """
+    A class to hold the Markov chain object for a multi-class dynamic classes priority MMc queue
+    """
+    def __init__(self, number_of_servers, arrival_rates, service_rates, class_change_rate_matrix, inf):
+        """
+        Initialises the Markov Chain object
+        """
+        self.c = number_of_servers
+        self.ls = arrival_rates
+        self.ms = service_rates
+        self.thetas = class_change_rate_matrix
+        self.inf = inf
+        self.k = len(arrival_rates)
+        self.State_Space = list(itertools.product(*[range(self.inf) for _ in range(self.k)]))
+        self.lenmat = len(self.State_Space)
+        self.write_transition_matrix()
+        self.discretise_transition_matrix()
+        self.solve()
+        self.aggregate_states()
+
+    def find_transition_rates(self, state1, state2):
+        """
+        Finds the transition rates for given state transition
+        """
+        delta = tuple(state2[i] - state1[i] for i in range(self.k))
+        if delta.count(0) == self.k - 1:
+            if delta.count(-1) == 1:
+                leaving_clss = delta.index(-1)
+                if all([state1[clss] == 0 for clss in range(leaving_clss)]):
+                    return self.ms[leaving_clss] * min(state1[leaving_clss], self.c)
+            elif delta.count(1) == 1:
+                arriving_clss = delta.index(1)
+                return self.ls[arriving_clss]
+        elif delta.count(0) == self.k - 2 and delta.count(1) == 1 and delta.count(-1) == 1:
+            leaving_clss = delta.index(-1)
+            arriving_clss = delta.index(1)
+            number_waiting = state1[leaving_clss] - min(self.c - min(self.c, sum(state1[:leaving_clss])), state1[leaving_clss])
+            return number_waiting * self.thetas[leaving_clss][arriving_clss]
+        return 0.0
+
+    def write_transition_matrix(self):
+        """
+        Writes the transition matrix for the markov chain
+        """
+        transition_matrix = np.array([[self.find_transition_rates(s1, s2) for s2 in self.State_Space] for s1 in self.State_Space])
+        row_sums = np.sum(transition_matrix, axis=1)
+        self.time_step = 1 / np.max(row_sums)
+        self.transition_matrix = transition_matrix - np.multiply(np.identity(self.lenmat), row_sums)
+
+    def discretise_transition_matrix(self):
+        """
+        Disctetises the transition matrix
+        """
+        self.discrete_transition_matrix = self.transition_matrix*self.time_step + np.identity(self.lenmat)
+
+    def solve(self):
+        A = np.append(np.transpose(self.discrete_transition_matrix) - np.identity(self.lenmat), [[1 for _ in range(self.lenmat)]], axis=0)
+        b = np.transpose(np.array([0 for _ in range(self.lenmat)] + [1]))
+        sol = np.linalg.solve(np.transpose(A).dot(A), np.transpose(A).dot(b))
+        self.probs =  {self.State_Space[i]: sol[i] for i in range(self.lenmat)}
+
+    def aggregate_states(self):
+        """
+        Aggregates from individual states to overall numbers of customers
+        """
+        agg_probs = {}
+        for state in self.probs.keys():
+            agg_state = sum(state)
+            if agg_state in agg_probs:
+                agg_probs[agg_state] += self.probs[state]
+            else:
+                agg_probs[agg_state] = self.probs[state]
+        self.aggregate_probs = agg_probs
+
+
+
+class Simulation:
+    """
+    A class to hold the Simulation for a multi-class dynamic classes priority MMc queue
+    """
+    def __init__(self, number_of_servers, arrival_rates, service_rates, class_change_rate_matrix, max_simulation_time, warmup):
+        self.max_simulation_time = max_simulation_time
+        self.warmup = warmup
+        self.obs_period = (self.warmup, self.max_simulation_time - self.warmup)
+        k = len(arrival_rates)
+        self.N = ciw.create_network(
+            arrival_distributions={'Class ' + str(c): [dists.Exponential(arrival_rates[c])] for c in range(k)},
+            service_distributions={'Class ' + str(c): [dists.Exponential(service_rates[c])] for c in range(k)},
+            number_of_servers=[number_of_servers],
+            class_change_time_distributions=[[dists.Exponential(rate) if rate is not None else None for rate in row] for row in class_change_rate_matrix],
+            priority_classes={'Class ' + str(c): c for c in range(k)}
+        )
+        self.run()
+        self.aggregate_states()
+    
+    def run(self):
+        """
+        Runs the simulation and finds the state probabilities
+        """
+        ciw.seed(0)
+        self.Q = ciw.Simulation(self.N, tracker=trackers.NodeClassMatrix())
+        self.Q.simulate_until_max_time(self.max_simulation_time)
+        self.probs = self.Q.statetracker.state_probabilities(observation_period=self.obs_period)
+
+    def aggregate_states(self):
+        """
+        Aggregates from individual states to overall numbers of customers
+        """
+        self.aggregate_probs = {}
+        for state in self.probs.keys():
+            agg_state = sum(s[0] for s in state)
+            if agg_state in self.aggregate_probs:
+                self.aggregate_probs[agg_state] += self.probs[state]
+            else:
+                self.aggregate_probs[agg_state] = self.probs[state]
