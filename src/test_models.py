e76f7858c2966e9aed828a6a9e1d7ccf57a2ac0c
Vince Knight <vincent.knight@gmail.com>
2022-05-18 12:25:56 +0100

    Run black.

--- src/test_models.py
+++ src/test_models.py
@@ -1,5 +1,6 @@
 import models
 
+
 def test_simulation_terminates():
     """
     TODO Document and add to this test.
@@ -8,13 +9,9 @@ def test_simulation_terminates():
         number_of_servers=1,
         arrival_rates=[5, 6],
         service_rates=[12, 13],
-        class_change_rate_matrix=[
-            [None, 3],
-            [1, None]
-        ],
-        preempt='resample',
+        class_change_rate_matrix=[[None, 3], [1, None]],
+        preempt="resample",
         max_simulation_time=200,
-        warmup=20
+        warmup=20,
     )
     assert round(S.probs[((0, 0),)], 2) == 0.12
-
