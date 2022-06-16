c4a2ae8b525fd8dd94b740a38a0409b2644147fa
Geraint Palmer <palmer.geraint@googlemail.com>
2022-06-16 16:01:17 +0100

    update tests to functions

--- src/test_models.py
+++ src/test_models.py
@@ -1,17 +1,36 @@
 import models
+import ciw
 
 
-def test_simulation_terminates():
+def test_simulation_builds_and_terminates():
     """
     TODO Document and add to this test.
     """
-    S = models.Simulation(
-        number_of_servers=1,
-        arrival_rates=[5, 6],
-        service_rates=[12, 13],
-        class_change_rate_matrix=[[None, 3], [1, None]],
-        preempt="resample",
-        max_simulation_time=200,
-        warmup=20,
+    max_simulation_time = 100
+    num_classes = 2
+    num_servers = 2
+    arrival_rates=[5, 5]
+    service_rates=[3, 4]
+    class_change_rate_matrix=[[None, 3], [2, None]]
+    Q = models.build_and_run_simulation(
+        num_classes=num_classes,
+        num_servers=num_servers,
+        arrival_rates=arrival_rates,
+        service_rates=service_rates,
+        class_change_rate_matrix=class_change_rate_matrix,
+        max_simulation_time=max_simulation_time
     )
-    assert round(S.probs[((0, 0),)], 2) == 0.12
+    inds = Q.get_all_individuals()
+    assert Q.current_time > max_simulation_time
+    assert isinstance(Q, ciw.Simulation)
+    assert len(Q.transitive_nodes) == 1
+    assert Q.nodes[1].c == num_servers
+    assert Q.service_times[1][0].rate == service_rates[0]
+    assert Q.service_times[1][1].rate == service_rates[1]
+    assert Q.inter_arrival_times[1][0].rate == arrival_rates[0]
+    assert Q.inter_arrival_times[1][1].rate == arrival_rates[1]
+    assert Q.network.customer_classes[0].class_change_time_distributions[0] == class_change_rate_matrix[0][0]
+    assert Q.network.customer_classes[0].class_change_time_distributions[1].rate == class_change_rate_matrix[0][1]
+    assert Q.network.customer_classes[1].class_change_time_distributions[0].rate == class_change_rate_matrix[1][0]
+    assert Q.network.customer_classes[1].class_change_time_distributions[1] == class_change_rate_matrix[1][1]
+    assert len(inds) > 0
