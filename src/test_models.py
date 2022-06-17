990f0808168259d4e6ef04772c6609851be2c8b6
Michalis <PanayidesM@cardiff.ac.uk>
2022-06-17 11:43:32 +0100

    Add test for pairs of non-zero entries states
    Include a test for a new function that is meant to be used to make
    things faster in generating the transition matrix

--- src/test_models.py
+++ src/test_models.py
@@ -90,3 +90,54 @@ def test_write_state_space_for_sojourn():
     assert min(min(s) for s in states[:-1]) == 0
     assert all(len(s) == n_classes + 2 for s in states[:-1])
     assert states[-1] == "*"
+
+
+def test_get_all_pairs_of_non_zero_entries_states_example():
+    """
+    Tests we get all expected pairs of states that have a possible non-zero
+    rate.
+    """
+    num_classes = 2
+    infty = 3
+    all_states = models.write_state_space_for_states(
+        num_classes=num_classes, infty=infty
+    )
+    all_pairs = models.get_all_pairs_of_non_zero_entries_states(
+        State_Space=all_states, infty=infty
+    )
+    expected_pairs = set(
+        ((0, 0), (1, 0)),
+        ((0, 0), (0, 1)),
+        ((0, 1), (1, 1)),
+        ((0, 1), (0, 2)),
+        ((0, 1), (0, 0)),
+        ((0, 1), (1, 0)),
+        ((0, 2), (1, 2)),
+        ((0, 2), (0, 1)),
+        ((0, 2), (1, 1)),
+        ((1, 0), (2, 0)),
+        ((1, 0), (0, 0)),
+        ((1, 0), (0, 1)),
+        ((1, 0), (1, 1)),
+        ((1, 1), (2, 1)),
+        ((1, 1), (0, 1)),
+        ((1, 1), (0, 2)),
+        ((1, 1), (1, 2)),
+        ((1, 1), (1, 0)),
+        ((1, 1), (2, 0)),
+        ((1, 2), (2, 2)),
+        ((1, 2), (0, 2)),
+        ((1, 2), (1, 1)),
+        ((1, 2), (2, 1)),
+        ((2, 0), (1, 0)),
+        ((2, 0), (1, 1)),
+        ((2, 0), (2, 1)),
+        ((2, 1), (1, 1)),
+        ((2, 1), (1, 2)),
+        ((2, 1), (2, 2)),
+        ((2, 1), (2, 0)),
+        ((2, 2), (1, 2)),
+        ((2, 2), (2, 1)),
+    )
+    assert len(all_pairs) == len(expected_pairs)
+    assert expected_pairs == set(all_pairs)
