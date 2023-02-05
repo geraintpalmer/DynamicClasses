1401c6e99a4da1067464edbc46e65c37a76b2969
Geraint Palmer <palmer.geraint@googlemail.com>
2023-02-05 18:39:14 +0000

    add script to get bound test data

---
+++ src/experiments/get_bound_plot_data.py
@@ -0,0 +1,100 @@
+import sys
+
+sys.path.append("..")
+import models
+import tqdm
+from csv import writer
+import multiprocessing
+import argparse
+
+
+def get_max_hitting_prob(
+    num_classes, num_servers, arrival_rates, service_rates, thetas, bound, rr
+):
+    (
+        state_space,
+        trans_matrix,
+    ) = models.build_state_space_and_transition_matrix_sojourn_mc(
+        num_classes=num_classes,
+        num_servers=num_servers,
+        arrival_rates=arrival_rates,
+        service_rates=service_rates,
+        thetas=thetas,
+        bound=bound,
+    )
+    max_hitting_prob = models.get_maximum_hitting_probs(
+        state_space=state_space,
+        transition_matrix=trans_matrix,
+        boundary=bound,
+        reasonable_ratio=rr,
+    )
+    return max_hitting_prob
+
+
+def write_row(params, bound):
+    p = get_max_hitting_prob(
+        num_classes=params["num_classes"],
+        num_servers=params["num_servers"],
+        arrival_rates=params["arrival_rates"],
+        service_rates=params["service_rates"],
+        thetas=params["thetas"],
+        bound=bound,
+        rr=0.8,
+    )
+
+    with open("bound_hitting_prob_data.csv", "a") as f:
+        writer_object = writer(f)
+        writer_object.writerow([params["example_name"], bound, p])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n_cores", help="Number of Cores to use")
+    args = parser.parse_args()
+    n_cores = int(args.n_cores)
+    bounds = list(range(6, 40))
+
+    ### Examples
+    example_A = {
+        "num_classes": 2,
+        "num_servers": 1,
+        "arrival_rates": [5, 6],
+        "service_rates": [12, 13],
+        "thetas": [[None, 3], [1, None]],
+        "example_name": "A",
+    }
+
+    example_B = {
+        "num_classes": 2,
+        "num_servers": 1,
+        "arrival_rates": [3, 6],
+        "service_rates": [12, 13],
+        "thetas": [[None, 2], [5, None]],
+        "example_name": "B",
+    }
+
+    example_C = {
+        "num_classes": 2,
+        "num_servers": 3,
+        "arrival_rates": [5, 6],
+        "service_rates": [3.8, 4],
+        "thetas": [[None, 1], [2, None]],
+        "example_name": "C",
+    }
+
+    example_D = {
+        "num_classes": 2,
+        "num_servers": 2,
+        "arrival_rates": [5, 6],
+        "service_rates": [6.5, 7.5],
+        "thetas": [[None, 2], [4, None]],
+        "example_name": "D",
+    }
+
+    pool = multiprocessing.Pool(n_cores)
+    func_arguments = [
+        (params, bound)
+        for bound in bounds
+        for params in [example_A, example_B, example_C, example_D]
+    ]
+    pool.starmap(write_row, func_arguments)
