1d03a146b2554281c85ddc8187d20f8a6d32acf3
Michalis <michalis.pan1@hotmail.com>
2023-03-17 17:30:28 +0000

    Minor adjustments to the script

--- src/experiments/main.py
+++ src/experiments/main.py
@@ -1,3 +1,13 @@
+"""
+This file is used to run the experiments for the paper. A parameter sweep is
+performed over a particular set of parameters. One of the parameters `arrival_1`
+is fixed and the other parameters are scaled by a factor. The scaling factors
+are given by the lists `arrival_2_proportions`, `service_1_proportions`,
+`service_2_proportions`, `theta_12_proportions`, `theta_21_proportions`. The
+number of servers is varied over the list `num_servers_space`. The results are
+written to the file `data.csv`.
+"""
+
 from csv import writer
 import itertools
 import sys
@@ -10,35 +20,39 @@ import pandas as pd
 import models
 
 num_classes = 2
-max_simulation_time = 1000
-warmup_time = 100
-cooldown_time = 100
+max_simulation_time = 20000
+warmup_time = 2000
+cooldown_time = 200
 
-arrival_1_space = np.linspace(1, 5, 5)
-arrival_2_space = np.linspace(1, 5, 5)
-service_1_space = np.linspace(1, 5, 5)
-service_2_space = np.linspace(1, 5, 5)
-num_servers_space = np.linspace(1, 5, 5, dtype=int)
-theta_12_space = np.linspace(1, 5, 5)
-theta_21_space = np.linspace(1, 5, 5)
+arrival_1 = 1
+arrival_2_proportions = [1/3, 1/2, 1, 2, 3]
+service_1_proportions = [1/3, 1/2, 1, 2, 3]
+service_2_proportions = [1/3, 1/2, 1, 2, 3]
+num_servers_space = [1, 2, 3]
+theta_12_proportions = [1/3, 1/2, 1, 2, 3]
+theta_21_proportions = [1/3, 1/2, 1, 2, 3]
 
 for (
-    arrival_1,
-    arrival_2,
-    service_1,
-    service_2,
+    arrival_2_ratio,
+    service_1_ratio,
+    service_2_ratio,
     num_servers,
-    theta_12,
-    theta_21,
+    theta_12_ratio,
+    theta_21_ratio,
 ) in itertools.product(
-    arrival_1_space,
-    arrival_2_space,
-    service_1_space,
-    service_2_space,
+    arrival_2_proportions,
+    service_1_proportions,
+    service_2_proportions,
     num_servers_space,
-    theta_12_space,
-    theta_21_space,
+    theta_12_proportions,
+    theta_21_proportions,
 ):
+    arrival_2 = arrival_1 * arrival_2_ratio
+    service_1 = arrival_1 * service_1_ratio
+    service_2 = arrival_1 * service_2_ratio
+    theta_12 = arrival_1 * theta_12_ratio
+    theta_21 = arrival_1 * theta_21_ratio
+    
     arrival_rates = [float(arrival_1), float(arrival_2)]
     service_rates = [float(service_1), float(service_2)]
     thetas = [[None, float(theta_12)], [float(theta_21), None]]
@@ -55,6 +69,6 @@ for (
         progress_bar=True,
     )
 
-    with open("test.csv", "a") as f:
+    with open("data.csv", "a") as f:
         writer_object = writer(f)
         writer_object.writerow(row)
